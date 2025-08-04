5302a2ec3fbc50f0a045343f8728559719712fd7
Vaibhav5771 <vaibhavpardeshi190@gmail.com>
2025-08-04 12:54:43 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,46 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import NavBar from "~/components/NavBar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router";
+import {usePuterStore} from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    const {  auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen">
+      <NavBar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+        {resumes.length > 0 ? (
+          <div className="resumes-section container mx-auto flex flex-wrap max-md:flex-col max-md:gap-4 gap-6 items-center max-md:items-center justify-center w-full max-w-[1850px] p-4">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        ) : (
+          <div className="resumes-section container mx-auto p-4 text-center">
+            <p>No resumes found. Start by adding one!</p>
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
